4b307a80c4a617f93356c6d3999f28a36031d202
OpenShift guest <hong8hong8@gmail.com>
2014-02-13 19:50:25 -0500

    commit

--- public_bak/js/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('intelli-dash', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'intelli-dash.system', 'intelli-dash.projects']);
-
-angular.module('intelli-dash.system', []);
-angular.module('intelli-dash.projects', []);
--- public_bak/js/controllers/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('intelli-dash.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
-    $scope.global = Global;
-}]);
