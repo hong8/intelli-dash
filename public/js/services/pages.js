96ef73465db85fb43a88a37b523a0f9355c349c4
OpenShift guest <hong8hong8@gmail.com>
2014-02-18 05:30:26 -0500

    commit

--- public/js/services/pages.js
+++ public/js/services/pages.js
@@ -1,8 +1,9 @@
 'use strict';
 
 //Pages service used for articles REST endpoint
-angular.module('intelli-dash.pages').factory('Pages', ['$resource', function($resource) {
-    return $resource('pages/:pageId', {
+angular.module('intelli-dash.pages').factory('Pages', ['$resource', '$routeParams', function($resource, $routeParams) {
+    return $resource('projects/:projectId/pages/:pageId', {
+        projectId: $routeParams.projectId,
         pageId: '@_id'
     }, {
         update: {
